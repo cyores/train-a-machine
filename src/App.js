593bc82e1d7d9a0dbac1e3a58c303887d9dbee55
Christian Yores <christianyores@gmail.com>
2019-09-15 19:03:23 -0400

    CSS boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>Hello, World!</h1>
+            <h2>Hello, World!</h2>
+            <h3>Hello, World!</h3>
+            <h4>Hello, World!</h4>
+            <h5>Hello, World!</h5>
+            <h6>Hello, World!</h6>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                mi nisi, ultrices a iaculis sit amet, sollicitudin vitae risus.
+                Vivamus tortor libero, hendrerit sit amet interdum id, ultrices
+                sit amet massa. Nullam cursus enim ac nisl vulputate, a
+                fringilla quam rhoncus. Phasellus ut elementum magna. Nunc vitae
+                scelerisque nibh. Nunc quis ex ac eros tempus condimentum. Duis
+                ut gravida quam. Donec eu tincidunt purus. Donec posuere auctor
+                lectus ac pulvinar. Nam aliquam porta velit, non consequat
+                tellus semper finibus. Nulla vestibulum rhoncus ex, laoreet
+                viverra dolor blandit at.
+            </p>
+            <small>small</small>
+        </>
+    );
 }
 
 export default App;
